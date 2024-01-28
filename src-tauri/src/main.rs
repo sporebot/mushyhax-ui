3f2da1d1a3c468b663761fec96b5ebf47a8bb03c
httpget <wyatt2086@proton.me>
2024-01-28 01:38:59 -0500

    yeah

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri::{PhysicalSize, Size};
+
+
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn executor_resize(window: tauri::Window) {
+    window.set_size(Size::Physical(PhysicalSize{width: 1000, height: 500}));
+    window.set_title("MSHX - Executor");
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![executor_resize])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import "./styles.css";
-import App from "./App.svelte";
+const { invoke } = window.__TAURI__.tauri;
 
-const app = new App({
-  target: document.getElementById("app"),
+let greetInputEl;
+let greetMsgEl;
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
 });
-
-export default app;
